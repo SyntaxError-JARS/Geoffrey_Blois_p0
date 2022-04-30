a5733fb7a3f4ce968e92958fa352339bc4cae100
Geoffrey <bloisgeoffrey@yahoo.com>
2022-04-30 11:41:59 -0500

    Added a way to add data that a user inputs into a file. Added a password check to make sure the user inputs the correct password twice. (Looking into other ways to varify information for emails and phone numbers)

--- bankApplicationP0/src/com/revature/bank_application/MainDriver.java
+++ bankApplicationP0/src/com/revature/bank_application/MainDriver.java
@@ -2,68 +2,88 @@ package com.revature.bank_application;
 
 import com.revature.bank_application.models.UserAccountData;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
+
 
 public class MainDriver {
 
     static BufferedReader terminalReader = new BufferedReader(new InputStreamReader(System.in));
+
     public static void main(String[] args) {
 
+        boolean isAppRunning  = true;
         // When the user start the application they will be greeted with this:
         String welcome = "Welcome to the Bank of Everything!";
         String option1 = " 1) Not a User? Create an Account";
         String option2 = " 2) A returning User? Sign in";
         String option3 = " 3) Leave the Bank of Everything";
 
-        System.out.printf("%s \n %s \n %s \n %s", welcome, option1, option2, option3).println();
+        while(isAppRunning == true) {
+            System.out.printf("%s \n %s \n %s \n %s", welcome, option1, option2, option3).println();
 
-        try {
-            System.out.println("Select one of the input from above\n >");
-            String userInputToEnterBankApp = terminalReader.readLine();
+            try {
+                System.out.println("Select one of the input from above\n >");
+                String userInputToEnterBankApp = terminalReader.readLine();
 
 
-            // This Will allow users to select an input.
-            switch (userInputToEnterBankApp) {
-                case "1":
-                    System.out.println("User has chosen to create an account ...");
-                    userCreateAccountInput();
-                    break;
-                case "2":
-                    System.out.println("User has chosen to Login. Welcome back User ...");
-                    userCreateAccountInput();
-                    break;
-                case "3":
-                    System.out.println("User has chosen to exit application. Thank for using our service ...");
-                    break;
-                default:
-                    System.out.println("A invalid input has been detected please run the application again and use one of the valid input. Thank You ...");
-                    break;
+                // This Will allow users to select an input.
+                switch (userInputToEnterBankApp) {
+                    case "1":
+                        System.out.println("User has chosen to create an account ...");
+                        userCreateAccountInput();
+                        break;
+                    case "2":
+                        System.out.println("User has chosen to Login. Welcome back User ...");
+                        userCreateAccountInput();
+                        break;
+                    case "3":
+                        System.out.println("User has chosen to exit application. Thank for using our service ...");
+                        break;
+                    default:
+                        System.out.println("A invalid input has been detected please run the application again and use one of the valid input. Thank You ...");
+                        break;
 
+                }
+
+            } catch (IOException e) {
+                throw new RuntimeException(e);
             }
 
-        }catch (IOException e){
-            throw  new RuntimeException(e);
         }
-
-
-
     }
+
     // This code will grab an exception and throw to the method above.
-    static void userCreateAccountInput() throws IOException{
+    static void userCreateAccountInput() throws IOException {
+
+
         System.out.println("Create a Username");
         String username = terminalReader.readLine();
         System.out.println("Create a Password");
         String password = terminalReader.readLine();
+        System.out.println("Create a Password");
+        String passwordCheck = terminalReader.readLine();
         System.out.println("Add your Phone Number");
         String phoneNumber = terminalReader.readLine();
         System.out.println("Add your email address");
         String email = terminalReader.readLine();
 
+
+        if (!password.equals(passwordCheck)) {
+            System.out.println("The Passwords you entered did not match. Please re-enter the information and make sure the passwords match.");
+            return;
+        }
+
         UserAccountData accountData = new UserAccountData(username, password, phoneNumber, email);
         System.out.println(accountData);
 
+        File userPersistingData = new File("data/user_Data.txt");
+
+        try (FileWriter fileWriter = new FileWriter(userPersistingData, true)) {
+            fileWriter.write(accountData.toFileString());
+        } catch (IOException e) {
+            e.printStackTrace();
+
+        }
     }
 
 }
--- bankApplicationP0/src/com/revature/bank_application/models/UserAccountData.java
+++ bankApplicationP0/src/com/revature/bank_application/models/UserAccountData.java
@@ -1,17 +1,21 @@
 package com.revature.bank_application.models;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class UserAccountData {
 
     // These are creating private strings that only this script can see.
-    private String userName;
-    private String password;
-    private String phoneNumber;
-    private String email;
+    private static String userName;
+    private static String password;
+    private static String phoneNumber;
+    private static String email;
 
-    public UserAccountData()
+    /*public UserAccountData()
     {
         super();
-    }
+    }*/
     // This method is grabbing the Strings above and assigning them
     public UserAccountData(String userName, String password, String phoneNumber, String email){
         super();
@@ -27,18 +31,48 @@ public class UserAccountData {
         return userName;
     }
 
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
     public String getPassword() {
         return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
     }
 
     public String getPhoneNumber() {
         return phoneNumber;
     }
 
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
     public String getEmail() {
         return email;
     }
 
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+
+    public String toFileString(){
+        StringBuilder mutableString = new StringBuilder();
+        mutableString
+                .append(userName)
+                .append(",")
+                .append(password)
+                .append(",")
+                .append(phoneNumber)
+                .append(",")
+                .append(email);
+
+        return mutableString.toString();
+    }
 
     // I guess that this checks to see if any data has been enter and if it has it will over right it otherwise it will return null or some default data.
     @Override
