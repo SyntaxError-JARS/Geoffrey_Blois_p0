0927254da406d11d5074bda9d2331368349d69c5
Geoffrey <bloisgeoffrey@yahoo.com>
2022-05-06 15:19:40 -0500

    Added a new exception for when a it does a insert sql statment and it comes back as 0 it will print that as an exception.

---
+++ bankApplicationP0/src/main/java/com/revature/bank_application/execeptions/BankAccountInsertDataException.java
@@ -0,0 +1,7 @@
+package main.java.com.revature.bank_application.execeptions;
+
+public class BankAccountInsertDataException extends RuntimeException{
+    public BankAccountInsertDataException(String message) {
+        super(message);
+    }
+}
