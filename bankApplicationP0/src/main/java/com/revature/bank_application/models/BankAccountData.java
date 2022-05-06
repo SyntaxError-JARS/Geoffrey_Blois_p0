f5a713a60388c8286cd1c2aba9e93332abdb4b8c
Geoffrey <bloisgeoffrey@yahoo.com>
2022-05-06 14:01:39 -0500

    Created a new class for my BankAccountData in the models package as it represents my bankaccount table.

---
+++ bankApplicationP0/src/main/java/com/revature/bank_application/models/BankAccountData.java
@@ -0,0 +1,62 @@
+package main.java.com.revature.bank_application.models;
+
+public class BankAccountData {
+
+     //File signInFile = new File("data/User_Data");
+    // TODO: Update this class to bank account class instead so the models package follows the amount of tables I have.
+
+    private int bankAccountNumber;
+    private String bankAccountName;
+    private int bankAccountAmount;
+    private boolean hasBankAccount;
+
+    public BankAccountData(int bankAccountNumber, String bankAccountName, int bankAccountAmount, boolean hasBankAccount){
+        super();
+        this.bankAccountNumber = bankAccountNumber;
+        this.bankAccountName = bankAccountName;
+        this.bankAccountAmount = bankAccountAmount;
+        this.hasBankAccount = hasBankAccount;
+    }
+
+    public int getBankAccountNumber() {
+        return bankAccountNumber;
+    }
+
+    public void setBankAccountNumber(int bankAccountNumber) {
+        this.bankAccountNumber = bankAccountNumber;
+    }
+
+    public String getBankAccountName() {
+        return bankAccountName;
+    }
+
+    public void setBankAccountName(String bankAccountName) {
+        this.bankAccountName = bankAccountName;
+    }
+
+    public int getBankAccountAmount() {
+        return bankAccountAmount;
+    }
+
+    // TODO: This should not be Here!!!!!!!!!!!!
+//    public void setBankAccountAmount(int bankAccountAmount) {
+//        this.bankAccountAmount = bankAccountAmount;
+//    }
+
+    public void getHasBankAccount(boolean hasBankAccount){
+        this.hasBankAccount = hasBankAccount;
+    }
+    public void setHasBankAccount(boolean hasBankAccount) {
+        this.hasBankAccount = hasBankAccount;
+    }
+
+    @Override
+    public String toString() {
+        return "BankAccountData{" +
+                "bankAccountNumber=" + bankAccountNumber +
+                ", bankAccountName='" + bankAccountName + '\'' +
+                ", bankAccountAmount=" + bankAccountAmount +
+                ", hasBankAccount=" + hasBankAccount +
+                '}';
+    }
+}
--- bankApplicationP0/src/main/java/com/revature/bank_application/models/SignInAccount.java
+++
@@ -1,7 +0,0 @@
-package com.revature.bank_application.models;
-
-public class SignInAccount {
-
-     //File signInFile = new File("data/User_Data");
-    // TODO: Update this class to bank account class instead so the models package follows the amount of tables I have.
-}
