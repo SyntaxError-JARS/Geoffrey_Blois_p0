f2d39210f288a9757109b43d081cc39c01c04b26
Geoffrey <bloisgeoffrey@yahoo.com>
2022-05-10 16:28:13 -0500

    Started creating methods for my transactionDoa class.

--- bankApplicationP0/src/main/java/com/revature/bank_application/daos/TransactionDoa.java
+++ bankApplicationP0/src/main/java/com/revature/bank_application/daos/TransactionDoa.java
@@ -1,4 +1,41 @@
 package com.revature.bank_application.daos;
 
-public class TransactionDoa {
+import com.revature.bank_application.models.TransactionData;
+import com.revature.bank_application.util.ConnectionFactory;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class TransactionDoa implements TransactionsDoaInterface<TransactionData>{
+    @Override
+    public TransactionData withDraw(int amount)  {
+        try(Connection conn = ConnectionFactory.getInstance().getConnection()){
+
+
+
+
+
+            // TODO: replace this!!!!!!!!!!!!!!!!!!!
+            return null;
+        }catch (SQLException e){
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    @Override
+    public TransactionData deposit(int object) {
+
+        try(Connection conn = ConnectionFactory.getInstance().getConnection()){
+
+
+
+
+            // TODO: replace this!!!!!!!!!!!!!!!!!!!
+            return null;
+        }catch (SQLException e){
+            e.printStackTrace();
+            return null;
+        }
+    }
 }
